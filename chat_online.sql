08d0cb6447af6e964a0edd9d23dbea15cdecf235
pol-mrk <90915799+pol-mrk@users.noreply.github.com>
2024-11-02 20:44:57 +0100

    bbdd actualizada

--- chat_online.sql
+++ chat_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 30-10-2024 a las 17:19:33
+-- Tiempo de generación: 02-11-2024 a las 19:44:02
 -- Versión del servidor: 8.3.0
 -- Versión de PHP: 8.2.18
 
@@ -36,9 +36,36 @@ CREATE TABLE IF NOT EXISTS `tbl_amigos` (
   `usuario2` int DEFAULT NULL,
   `estado` enum('solicitado','rechazado','amigo') DEFAULT NULL,
   PRIMARY KEY (`id_amigo`),
+  UNIQUE KEY `relacion_unica_amigos` (`usuario1`,`usuario2`),
   KEY `usuario1_fk_idx` (`usuario1`),
   KEY `usuario2_fk_idx` (`usuario2`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_amigos`
+--
+
+INSERT INTO `tbl_amigos` (`id_amigo`, `usuario1`, `usuario2`, `estado`) VALUES
+(12, 1, 2, 'amigo'),
+(16, 1, 3, 'amigo'),
+(18, 1, 5, 'solicitado'),
+(19, 2, 5, 'amigo'),
+(24, 2, 3, 'rechazado');
+
+--
+-- Disparadores `tbl_amigos`
+--
+DROP TRIGGER IF EXISTS `ordenar_ids_de_menor_a_mayor`;
+DELIMITER $$
+CREATE TRIGGER `ordenar_ids_de_menor_a_mayor` BEFORE INSERT ON `tbl_amigos` FOR EACH ROW BEGIN
+    IF NEW.usuario1 > NEW.usuario2 THEN
+        SET @temp = NEW.usuario1;
+        SET NEW.usuario1 = NEW.usuario2;
+        SET NEW.usuario2 = @temp;
+    END IF;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -56,7 +83,21 @@ CREATE TABLE IF NOT EXISTS `tbl_mensajes` (
   PRIMARY KEY (`id_mensaje`),
   KEY `usuario_emisor_fk_idx` (`emisor`),
   KEY `usuario_receptor_fk_idx` (`receptor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_mensajes`
+--
+
+INSERT INTO `tbl_mensajes` (`id_mensaje`, `emisor`, `receptor`, `mensaje_chat`, `fecha_chat`) VALUES
+(1, 1, 2, 'sdgftrh', '2024-10-30 21:23:27'),
+(2, 2, 1, 'erhty', '2024-10-30 20:09:01'),
+(4, 2, 1, 'sxdcfgt', '2024-10-30 21:27:17'),
+(5, 1, 2, 'WEFER', '2024-10-30 21:28:20'),
+(6, 1, 2, 'WEFER', '2024-10-30 21:28:50'),
+(22, 1, 2, 'ertrtrtg', '2024-11-02 18:49:42'),
+(23, 2, 1, 'ergrtg', '2024-11-02 18:49:48'),
+(24, 2, 1, 'ergert', '2024-11-02 18:49:57');
 
 -- --------------------------------------------------------
 
@@ -71,16 +112,21 @@ CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
   `apellidos` varchar(35) DEFAULT NULL,
   `email` varchar(75) DEFAULT NULL,
   `contrasena` varchar(255) DEFAULT NULL,
-  `estado` enum('Desconectado','En línea') DEFAULT NULL,
+  `estado` enum('desconectado','en línea') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_usuarios`
 --
 
 INSERT INTO `tbl_usuarios` (`id_usuario`, `nombre`, `apellidos`, `email`, `contrasena`, `estado`) VALUES
-(2, 'PolMarc', 'Montero Roca', 'polmarc.monro@gmail.com', '$2y$10$jnbvSB1LuDLrqQ/hWxmBVeqGqMYqq5M4gdDPji5KApX.07IQ52yiS', 'En línea');
+(1, 'PolMarc', 'Montero Roca', 'polmarc.monro@gmail.com', '$2y$10$jnbvSB1LuDLrqQ/hWxmBVeqGqMYqq5M4gdDPji5KApX.07IQ52yiS', 'en línea'),
+(2, 'Marcolo', 'Colome Cuenca', 'marcolo@gmail.com', '$2y$10$dUeBIuF0dUnNJaxWc6Xww.YpbeK8j.ZdLnUrQQklpEgdChUO.54tS', 'en línea'),
+(3, 'Sergi', 'Masip Manchado', 'sergi.masip@gmail.com', '$2y$10$dUeBIuF0dUnNJaxWc6Xww.YpbeK8j.ZdLnUrQQklpEgdChUO.54tS', 'en línea'),
+(4, 'Marioto', 'Ruiz Camuñas', 'marioto.ruiz@gmail.com', '$2y$10$dUeBIuF0dUnNJaxWc6Xww.YpbeK8j.ZdLnUrQQklpEgdChUO.54tS', 'en línea'),
+(5, 'David', 'Hompanera Campos', 'david.hompanera@gmail.com', '$2y$10$dUeBIuF0dUnNJaxWc6Xww.YpbeK8j.ZdLnUrQQklpEgdChUO.54tS', 'en línea'),
+(6, 'Iker', 'Luna Cruz', 'iker.luna@gmail.com', '$2y$10$dUeBIuF0dUnNJaxWc6Xww.YpbeK8j.ZdLnUrQQklpEgdChUO.54tS', 'en línea');
 
 --
 -- Restricciones para tablas volcadas
